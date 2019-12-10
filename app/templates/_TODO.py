27a16d04caabe200258fb7acae6d0ba70be60789
Eik <josheikenberry@gmail.com>
2019-12-10 01:46:12 -0500

    initial

---
+++ app/templates/_TODO.py
@@ -0,0 +1,13 @@
+"""
+Functionlity needed:
+random band name page (rand function pulls single band from db) **DONE
+upvote band name (add +1 to db upvote tally) **DONE
+unique band name check **DONE
+
+find a way to hash band ids to make them obfuscated in urls etc.
+report band name (press button to report band name, blacklist=1)
+admin page (reuse flask tutorial's login logic to make admin page)
+admin page - whitelist reported band name
+admin page - blacklist reported band name (don't delete to prevent it from being submitted again
+
+"""
