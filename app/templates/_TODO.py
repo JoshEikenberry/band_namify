e4e1a58bc163b878ccb401847bbcec8b60b7e056
Eik <josheikenberry@gmail.com>
2019-12-10 02:17:33 -0500

    initial

--- app/templates/_TODO.py
+++ app/templates/_TODO.py
@@ -1,13 +1,23 @@
 """
 Functionlity needed:
 random band name page (rand function pulls single band from db) **DONE
-upvote band name (add +1 to db upvote tally) **DONE
+upvote endpoint **DONe
 unique band name check **DONE
+blacklist endpoint **DONE
+whitelist endpoint **DONE
+report endpoint **DONE
+upvote endpoint **DONE
+report button on page **DONE
+upvote button on page **DONE
 
+hide blacklist/whitelist endpoints behind admin login
 find a way to hash band ids to make them obfuscated in urls etc.
-report band name (press button to report band name, blacklist=1)
-admin page (reuse flask tutorial's login logic to make admin page)
-admin page - whitelist reported band name
-admin page - blacklist reported band name (don't delete to prevent it from being submitted again
+admin pages (reuse flask tutorial's login logic to make admin page)
+
+admin blacklist page (sort by reports and blacklist status)
+admin whitelist page (sort by reports and whitelist status)
+top votes page
+
+
 
 """
